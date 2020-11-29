38c81a0fc32ef4c3873aabb182bb671d454890f0
yanis fourel <yanis.fourel@epitech.eu>
2020-11-29 10:27:25 +0900

    Added light attenuation

--- CG_Engine/include/CG/internal/Shaders.hpp
+++ CG_Engine/include/CG/internal/Shaders.hpp
@@ -18,20 +18,30 @@ flat in vec3 f_pointLightColor;
 
 out vec4 color;
 
+float lightAttenuationFactor(float dist)
+{
+    return 1 / pow(dist, 2);
+}
+
 void main()
 {
-    const float shininess = 0.8;
+    const float shininess = 2;
 
 
     const vec3 L = normalize(vec3(f_pointLightPosition) - f_surfacePosition);
     const vec3 R = reflect(-L, f_surfaceNormal);
-    
+
+    const float distanceSurfaceLight = distance(f_pointLightPosition, f_surfacePosition);
+    const float distanceSurfaceEye = length(f_surfacePosition);
+
+    const float diffuseLightColorAttenuation = lightAttenuationFactor(distanceSurfaceLight);
+    const float specularLightColorAttenuation = lightAttenuationFactor(distanceSurfaceLight + distanceSurfaceEye);
 
     const float dotLN = max(0, dot(f_surfaceNormal, L));
     const float dotRPos = max(0, dot(R, -f_surfacePosition));
     
-    const vec3 diffuseLightColor = f_pointLightColor * dotLN;
-    const vec3 specularLightColor = f_pointLightColor * pow(dotRPos, shininess);
+    const vec3 diffuseLightColor =  diffuseLightColorAttenuation * f_pointLightColor * dotLN;
+    const vec3 specularLightColor = specularLightColorAttenuation * f_pointLightColor * pow(dotRPos, shininess);
 
     color = f_surfaceColor * vec4(f_ambiantLightColor + diffuseLightColor + specularLightColor, 1.0);
 }
